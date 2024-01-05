fd195b1a529063eb09ad2c3be6db6aa5aed13d6e
Ashutosh Pratap Singh <023.ashutosh@gmail.com>
2024-01-06 02:45:17 +0530

    Minor : Fetch Api results

--- src/components/News.js
+++ src/components/News.js
@@ -1,20 +1,37 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news component.
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+      <div className="container my-3">
+        <h4>
+          <b>NewsX</b> : Top Headlines
+        </h4>
+
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem
+              title="Cricket"
+              description="Lorem IpsumLorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum  "
+            />
+          </div>
+          <div className="col-md-4">
+            <NewsItem
+              title="Cricket"
+              description="Lorem IpsumLorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum  "
+            />
+          </div>
+          <div className="col-md-4">
+            <NewsItem
+              title="Cricket"
+              description="Lorem IpsumLorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum  "
+            />
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+
+    let {title , description} = this.props;
+
     return (
       <div>
-        This is a news item2.
+        <div className="card" style={{width: "18rem"}}>
+          <img src="https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/7E03/production/_132195223_gettyimages-1801895648.jpg" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">
+              {description}
+            </p>
+            <a href="/newsItem" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
