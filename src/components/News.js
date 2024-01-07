f0ea7117a53eea10c5e8ef3bf7c1ca4b689617e2
Ashutosh Pratap Singh <023.ashutosh@gmail.com>
2024-01-08 02:34:22 +0530

    Minor : fetch API implemented

--- src/components/News.js
+++ src/components/News.js
@@ -2,79 +2,56 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
-  articles = [
-    {
-      source: { id: "bbc-sport", name: "BBC Sport" },
-      author: null,
-      title: "McIlroy open to LIV if it becomes 'IPL of golf'",
-      description:
-        "Rory McIlroy says he would be open to play in a tournament backed by LIV Golf if the controversial tour became more like cricket's Indian Premier League.",
-      url: "http://www.bbc.co.uk/sport/golf/67870607",
-      urlToImage:
-        "https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/7E03/production/_132195223_gettyimages-1801895648.jpg",
-      publishedAt: "2024-01-03T12:07:18.8099575Z",
-      content:
-        "McIlroy was one of the strongest opponents when LIV was first announced but has since admitted it has brought some benefits\r\nRory McIlroy says he would be open to play in a tournament backed by LIV G… [+3198 chars]",
-    },
-    {
-      source: { id: "espn-cric-info", name: "ESPN Cric Info" },
-      author: null,
-      title:
-        "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      description:
-        "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      publishedAt: "2020-04-27T11:41:47Z",
-      content:
-        "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
-    },
-    {
-      source: { id: "espn-cric-info", name: "ESPN Cric Info" },
-      author: null,
-      title:
-        "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      description:
-        "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      publishedAt: "2020-03-30T15:26:05Z",
-      content:
-        "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
-    },
-  ];
+ 
 
   constructor() {
     super();
     this.state = {
-      articles: this.articles,
+      articles: [],
       loading: false,
     };
+  }
+
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/everything?q=global&apiKey=896903060ae545d899f1f44b8f469f8a";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({ articles: parsedData.articles });
   }
 
   render() {
     return (
       <div className="container my-3">
-        <h4>
+        <h2>
           <b>NewsX</b> : Top Headlines
-        </h4>
-        <div className="row">
+        </h2>
+        <div className="row my-4">
           {this.state.articles.map((element) => {
             return (
               <div className="col-md-4" key={element.url}>
                 <NewsItem
-                  title={element.title.slice(0,45)}
-                  description={element.description.slice(0,88)}
-                  image_url={element.urlToImage}
+                  title={
+                    element.title
+                      ? element.title.slice(0, 45)
+                      : "Click on Read More"
+                  }
+                  description={
+                    element.description
+                      ? element.description.slice(0, 88)
+                      : "Click on Read More"
+                  }
+                  image_url={
+                    element.urlToImage
+                      ? element.urlToImage
+                      : "https://img-cdn.tnwcdn.com/image/tnw-blurple?filter_last=1&fit=1280%2C640&url=https%3A%2F%2Fcdn0.tnwcdn.com%2Fwp-content%2Fblogs.dir%2F1%2Ffiles%2F2023%2F12%2Fglobalmobility-e1703163589861.jpeg&signature=8a6266fe80c1d552ccde0aed4beafcdb"
+                  }
                   news_url={element.url}
                 />
               </div>
             );
           })}
-
-
         </div>
       </div>
     );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,23 +1,26 @@
 import React, { Component } from "react";
+import "../App.css";
 
 export class NewsItem extends Component {
-
-  
-
   render() {
-
-    let {title , description, image_url, news_url} = this.props;
+    let { title, description, image_url, news_url } = this.props;
 
     return (
       <div className="my-3">
-        <div className="card" style={{width: "18rem"}}>
-          <img src={image_url} className="card-img-top" alt="..." />
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src={image_url}
+            className="card-img-top news_image_size"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
-            <p className="card-text">
-              {description}...
-            </p>
-            <a href={news_url} target="_blank" className="btn btn-sm btn-primary">
+            <p className="card-text">{description}...</p>
+            <a
+              href={news_url}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
               Read More
             </a>
           </div>
@@ -26,5 +29,5 @@ export class NewsItem extends Component {
     );
   }
 }
- 
+
 export default NewsItem;
